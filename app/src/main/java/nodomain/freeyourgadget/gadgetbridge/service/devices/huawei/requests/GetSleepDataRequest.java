98431eebe30cc1de1c949bfd869f491871a06418
Martin.JM <>
2022-03-13 10:43:34 +0100

    Huawei sleep loop patch

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/huawei/requests/GetSleepDataRequest.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/huawei/requests/GetSleepDataRequest.java
@@ -43,40 +43,40 @@ public class GetSleepDataRequest extends Request {
 
     @Override
     protected void processResponse() throws GBException {
-        HuaweiTLV container = receivedPacket.tlv.getObject(MessageData.responseContainerTag);
-        short receivedCount = container.getShort(MessageData.responseContainerNumberTag);
+        HuaweiTLV parentContainer = receivedPacket.tlv.getObject(MessageData.responseContainerTag);
+        short receivedCount = parentContainer.getShort(MessageData.responseContainerNumberTag);
 
         if (receivedCount != this.count) {
             LOG.warn("Counts do not match");
         }
 
-        container = container.getObject(FitnessData.MessageData.sleepResponseContainerContainerTag);
+        for (HuaweiTLV container : parentContainer.getObjects(FitnessData.MessageData.sleepResponseContainerContainerTag)) {
+            byte type = container.getByte(FitnessData.MessageData.sleepResponseContainerContainerDataTag);
 
-        byte type = container.getByte(FitnessData.MessageData.sleepResponseContainerContainerDataTag);
+            byte[] timestampBytes = container.getBytes(FitnessData.MessageData.sleepResponseContainerContainerTimestampTag);
 
-        byte[] timestampBytes = container.getBytes(FitnessData.MessageData.sleepResponseContainerContainerTimestampTag);
+            int[] timestampInts = new int[6];
 
-        int[] timestampInts = new int[6];
+            for (int i = 0; i < 6; i++) {
+                if (timestampBytes[i] >= 0)
+                    timestampInts[i] = timestampBytes[i];
+                else
+                    timestampInts[i] = timestampBytes[i] & 0xFF;
+            }
 
-        for (int i = 0; i < 6; i++) {
-            if (timestampBytes[i] >= 0)
-                timestampInts[i] = timestampBytes[i];
-            else
-                timestampInts[i] = timestampBytes[i] & 0xFF;
+            int timestamp =
+                    (timestampInts[0] << 24) +
+                            (timestampInts[1] << 16) +
+                            (timestampInts[2] << 8) +
+                            (timestampInts[3]);
+
+            int durationInt =
+                    (timestampInts[4] << 8L) +
+                            (timestampInts[5]);
+            short duration = (short) (durationInt * 60);
+
+            this.support.addSleepActivity(timestamp, duration, type);
         }
-
-        int timestamp =
-                (timestampInts[0] << 24) +
-                        (timestampInts[1] << 16) +
-                        (timestampInts[2] << 8) +
-                        (timestampInts[3]);
-
-        int durationInt =
-                (timestampInts[4] << 8L) +
-                        (timestampInts[5]);
-        short duration = (short)(durationInt * 60);
-
-        this.support.addSleepActivity(timestamp, duration, type);
 
         if (count + 1 < maxCount) {
             GetSleepDataRequest nextRequest = new GetSleepDataRequest(this.support, this.maxCount, (short) (this.count + 1));
